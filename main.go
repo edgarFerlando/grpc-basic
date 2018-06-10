42051fbfac7fb41be49ac1cff00690446ded1f65
fendijatmikon <jatmikofendi@gmail.com>
2018-06-10 20:42:35 +0700

    update main

--- main.go
+++ main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("vim-go")
+
+	v := "i m a string dude!!"
+	describe(v)
+
+	nyobi := false
+	describe(nyobi)
+}
+
+func describe(i interface{}) {
+	fmt.Printf("%v, %T\n", i, i)
 }
